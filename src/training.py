e8cee3cb0fe975878ac19aa785d882f80ca5349c
Susmithareddy0 <168408561+Susmithareddy0@users.noreply.github.com>
2026-03-06 13:37:35 +0530

    training

--- src/training.py
+++ src/training.py
@@ -1,1 +1,31 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from model import create_model
 
+train_path = "dataset/train"
+val_path = "dataset/validation"
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_path,
+    target_size=(224,224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+val_generator = train_datagen.flow_from_directory(
+    val_path,
+    target_size=(224,224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+model = create_model()
+
+model.fit(
+    train_generator,
+    validation_data=val_generator,
+    epochs=10
+)
+
+model.save("vitamin_model.h5")
