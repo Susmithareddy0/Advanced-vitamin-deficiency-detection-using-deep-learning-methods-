695c198de6596c09d1aace99d798b8d4c88efcd8
Susmithareddy0 <168408561+Susmithareddy0@users.noreply.github.com>
2026-03-06 13:35:55 +0530

    Update prediction.py

--- src/prediction.py
+++ src/prediction.py
@@ -1,1 +1,18 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
 
+model = tf.keras.models.load_model("vitamin_model.h5")
+
+classes = ['Vitamin A Deficiency', 'Vitamin B12 Deficiency', 'Vitamin D Deficiency', 'Healthy']
+
+img = image.load_img("test_image.jpg", target_size=(224,224))
+img_array = image.img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)
+img_array = img_array / 255.0
+
+prediction = model.predict(img_array)
+
+result = classes[np.argmax(prediction)]
+
+print("Prediction:", result)
